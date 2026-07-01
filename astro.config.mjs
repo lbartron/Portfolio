1493ab134cecbf5f27fb52f2b182f5d34fbead2c
Leo Bartron <leonb909@csslab15.uwb.edu>
2026-07-01 14:00:41 -0700

    Added rehype mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from 'astro-icon';
 import { unified } from '@astrojs/markdown-remark';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import rehypeMermaid from 'rehype-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,5 +18,10 @@ export default defineConfig({
       remarkPlugins: [remarkMath],
       rehypePlugins: [rehypeKatex],
     }),
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid'], // Prevents Astro from highlighting Mermaid code
+    },
+    rehypePlugins: [rehypeMermaid], // Adds the diagram tool
   },
 });
